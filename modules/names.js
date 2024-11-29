a65f62d14df0cf65580f043ba00bf7c71980e6e4
eleonoracappellesso <cappellessoeleonora@gmail.com>
2024-11-29 16:36:28 +0100

    created all files

---
+++ modules/names.js
@@ -0,0 +1,4 @@
+/*
+1. Crea una funzione che accetta due parametri: firstName, lastName. La funzione dovrebbe restituire un oggetto con le proprietà firstName e lastName.
+2. Esporta la funzione dal file.
+*/
