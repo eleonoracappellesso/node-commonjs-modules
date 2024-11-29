4d849b8d4e2174330ef76423ffb57ff195bb8eae
eleonoracappellesso <cappellessoeleonora@gmail.com>
2024-11-29 17:58:00 +0100

    created new variables with process.argv

--- people.js
+++ people.js
@@ -6,16 +6,28 @@
 */
 
 const userFullName = require("./names");
-//console.log(userFullName('Eleonora', 'Cappellesso'));
-
 const userHobbies = require("./hobbies");
-//console.log(userHobbies('pallavolo', 'cinema', 'musica'));
+
+// function userInfos() {
+//     const fullName = userFullName('Eleonora', 'Cappellesso');
+//     const hobbies = userHobbies('volleyball', 'movies', 'travel');
+
+//     return { fullName, hobbies };
+// }
+
+// console.log(userInfos());
+
 
 function userInfos() {
-    const fullName = userFullName('Eleonora', 'Cappellesso');
-    const hobbies = userHobbies('volleyball', 'movies', 'travel');
+    const firstName = process.argv[2] ?? 'Eleonora';
+    const lastName = process.argv[3] ?? 'Cappellesso';
+    const hobbyOne = process.argv[4] ?? 'volley';
+    const hobbyTwo = process.argv[5] ?? 'movies';
+    const hobbyThree = process.argv[6] ?? 'travel';
+
+    const fullName = userFullName(firstName, lastName);
+    const hobbies = userHobbies(hobbyOne, hobbyTwo, hobbyThree);
 
     return { fullName, hobbies };
 }
-
 console.log(userInfos());
