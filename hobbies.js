d58bbf463bc20ac1b292f952131d95c8cbb371f1
eleonoracappellesso <cappellessoeleonora@gmail.com>
2024-11-29 16:58:02 +0100

    created function hobbies in hobbies.js and exported it in people.js

---
+++ hobbies.js
@@ -0,0 +1,11 @@
+/*
+2 - hobbies.js
+1. Crea una funzione che accetta tre parametri: hobbyOne, hobbyTwo, hobbyThree. La funzione dovrebbe restituire un oggetto con una proprietà hobbies, che è un array di hobby.
+2. Esporta la funzione dal file.
+*/
+
+function hobbies(hobbyOne, hobbyTwo, hobbyThree) {
+    return { hobbies: [hobbyOne, hobbyTwo, hobbyThree] };
+}
+
+module.exports = hobbies;
--- people.js
+++ people.js
@@ -1,3 +1,13 @@
-const fullName = require("./names");
+/*
+3 - people.js
+1. Importa la tua funzione da names.js
+2. Importa la tua funzione da hobbies.js
+3. Crea una funzione che non ha parametri. La funzione dovrebbe restituire un oggetto con due proprietà: fullName ed hobbies. All'interno della tua funzione, usa le tue due funzioni precedenti per costruire l'oggetto.
+*/
 
-console.log(fullName('Eleonora', 'Cappellesso'));
+const userFullName = require("./names");
+//console.log(userFullName('Eleonora', 'Cappellesso'));
+
+const userHobbies = require("./hobbies");
+//console.log(userHobbies('pallavolo', 'cinema', 'musica'));
+
